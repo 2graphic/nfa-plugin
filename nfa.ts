5521cffb695fad9ae401613aba66e33f26beb9e6
Sheyne Anderson <sheyne@sheyne.com>
2017-04-18 01:05:17 -0600

    fix bugs with lambda transitions

--- nfa.ts
+++ nfa.ts
@@ -47,16 +47,12 @@ function isEmpty(label?: string) {
 }
 
 export function start(input: NFAGraph, data: string): State | boolean {
-    let start: NFANode | null = null;
+    let starts = new Set<NFANode>();
     const accepts = new Set<NFANode>();
 
     for (const node of input.nodes) {
         if (node.isStartState) {
-            if (!start) {
-                start = node;
-            } else {
-                throw new Error("Only one start state allowed");
-            }
+            starts.add(node);
         }
 
         if (node.isAcceptState) {
@@ -74,27 +70,50 @@ export function start(input: NFAGraph, data: string): State | boolean {
         }
     }
 
-    if (!start) {
-        throw new Error("Must have one start state");
+    if (starts.size === 0) {
+        throw new Error("Must have at least one start state");
     }
     if (accepts.size === 0) {
         throw new Error("Must have at least one accept state");
     }
 
-    return new State([start], data);
+    const initialActive = new Set<NFANode>();
+    for (const state of starts) {
+        visitNode(state, initialActive);
+    }
+    console.log("initialActive", initialActive);
+
+    return new State([...initialActive], data);
+}
+
+
+function visitNode(node: NFANode, destinations: Set<NFANode>) {
+    destinations.add(node);
+    for (const edge of node.children) {
+        if (edge.symbol === "") {
+            if (!destinations.has(edge.destination)) {
+                visitNode(edge.destination, destinations);
+            }
+        }
+    }
 }
 
 export function step(current: State): State | boolean {
     if (current.inputLeft.length === 0) {
         return current.active.reduce((a, b) => b.isAcceptState === true || a, false);
     }
-    const destinations = current.active.reduce((dests, a) => dests.concat(a.children), [] as NFAEdge[])
-        .filter(edge => edge.symbol === "" || edge.symbol === current.inputLeft[0])
-        .map(edge => edge.destination);
+    const destinations = new Set<NFANode>();
+    for (const activeNode of current.active) {
+        for (const edge of activeNode.children) {
+            if (edge.symbol === current.inputLeft[0]) {
+                visitNode(edge.destination, destinations);
+            }
+        }
+    }
 
-    if (destinations.length === 0) {
+    if (destinations.size === 0) {
         return false;
     } else {
-        return new State(destinations, current.inputLeft.substr(1));
+        return new State([...destinations], current.inputLeft.substr(1));
     }
 }
